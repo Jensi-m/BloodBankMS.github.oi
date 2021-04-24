4036ea76316d15ccd4fb2115c34be9ce82abc142
jensi <jensivinod@icloud.com>
2021-04-24 17:14:54 +0530

    Blood Bank Manegement System Project

---
+++ BDManagement/users/about.php
@@ -0,0 +1,24 @@
+<?php
+$title="About Us";$setAboutActive="active";
+include 'layout/_header.php';
+
+include 'layout/navbar.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <a href="#" id="Heading">About Us</a>
+            <br><br>
+            <p1>The researchers aimed to develop and implement an online blood bank management system. This web-based application allows hospitals to make inventories of their blood bags online, thus, allowing each hospital to check the availability of blood bags anytime. Likewise, proper accounting of blood donors ensures that the expected blood transfusion services will be safe and secured. </p1>
+            <br>
+            <br>
+            <br>
+            <p2>Blood transfusion safety remains an important public health concern in India. The availability of blood products of all blood types and the provision of its safety ensure public trust of its excellent healthcare system. However, lack of availability of these blood products and provision of unsafe blood products still impact morbidity and mortality in the Sultanate. Through the use of online blood bank management system, blood transfusion safety is expected to be enhanced or improved. Risks on improper blood donors’ documentation, and misplaced records can be minimized or totally avoided. Also, processes involving blood bag collection, storage, and inventory will be systematized and organized, hence, improving the healthcare management.
+</p2>
+
+        </div>
+    </div>
+</div>
+
+
